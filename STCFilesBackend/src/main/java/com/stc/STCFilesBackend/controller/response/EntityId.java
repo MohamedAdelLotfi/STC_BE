70bf9168d65a25f16ff31eb1c366fdb3f48403b8
Mohamed Adel <72199655+MohamedAdelLotfi@users.noreply.github.com>
2023-08-26 21:02:25 +0300

    Initial Commit for Files Backend

---
+++ STCFilesBackend/src/main/java/com/stc/STCFilesBackend/controller/response/EntityId.java
@@ -0,0 +1,13 @@
+package com.stc.STCFilesBackend.controller.response;
+
+import lombok.Data;
+
+@Data
+public class EntityId<T> {
+
+    private T id;
+
+    public EntityId() {
+        //
+    }
+}
